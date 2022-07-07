f557f1dda200bf7d12fb0b5f58f5e832aacc3385
srhodes <amplifier339@comcast.net>
2022-07-06 18:02:05 -0700

    upload files part 2

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'public/images')
     },
-    filename: (req, file, cd) => {
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
 })
